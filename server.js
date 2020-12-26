069f270691422d87cdcc0592aaa6a3ca37b5edc5
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-26 13:38:24 -0500

    updated server.js

--- server.js
+++ server.js
@@ -27,16 +27,14 @@ app.use('/credentials',credentialRouter);
 
 
   // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+  if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(port,()=> {
   console.log(`Server is running on port: ${port}`);
