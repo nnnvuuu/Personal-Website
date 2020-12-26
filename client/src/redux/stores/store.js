c0ea65cddde0e80171b4b27db87c9e60ab178dc0
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-26 17:49:21 -0500

    bug fix

--- client/src/redux/stores/store.js
+++ client/src/redux/stores/store.js
@@ -6,7 +6,7 @@ const initState ={};
 const middleware = [thunk];
 const store = createStore(rootReducer, initState, compose(
   applyMiddleware(...middleware),
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   
 ));
 
