6954c4ec8da9a228a5f37de3c20a6697ef733661
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-26 20:35:29 -0500

    Update LoginPage.js

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -90,7 +90,7 @@ function LoginPage(props) {
       props.clearErrors();
       window.location.href ="www.nenanou.com/home";
       // document.location.href = '/home',true;
-     
+        return;
     }
 
 
