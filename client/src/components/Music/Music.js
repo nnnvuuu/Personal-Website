cd72cd9dc6383702687518318159b6af6743c9e3
Zhixin chen <zhixin.chen72@gmail.com>
2021-12-13 18:05:07 -0500

    Update Music.js

--- client/src/components/Music/Music.js
+++ client/src/components/Music/Music.js
@@ -22,42 +22,38 @@ const audioList1 = [
   },
 
   
+  {
+    name: '2',
+    singer: "2",
+    musicSrc:
+      'https://res.cloudinary.com/dgemakazn/video/upload/v1639436302/zz/-_---__---__-_-____tqlotj.mp3'
+  
+  },
+  
 
  {
-  name: '2',
-  singer: "2",
+  name: '3',
+  singer: "3",
   musicSrc:
     'https://res.cloudinary.com/dgemakazn/video/upload/v1638916778/zz/%E6%A2%A6%E6%A0%B8dreamcore_%E4%BB%A4%E4%BA%BA%E6%80%80%E6%97%A7%E5%8F%88%E6%A2%A6%E5%B9%BB%E7%9A%84%E5%9B%BE%E7%89%87._%E6%A2%A6%E6%A0%B8dreamcore_%E4%BB%A4%E4%BA%BA%E6%80%80%E6%97%A7%E5%8F%88%E6%A2%A6%E5%B9%BB%E7%9A%84%E5%9B%BE%E7%89%87.422870703_jmmnri.mp3'
 
 },
 
 
-
-{
-  name: '3',
-  singer: "3",
-  musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638916774/zz/%E4%BC%A4%E6%A0%B8Traumacore_%E8%AE%A9%E4%BA%BA%E5%BC%95%E8%B5%B7%E7%AB%A5%E5%B9%B4%E5%88%9B%E4%BC%A4%E7%9A%84%E5%9B%BE%E7%89%87._%E4%BC%A4%E6%A0%B8Traumacore_%E8%AE%A9%E4%BA%BA%E5%BC%95%E8%B5%B7%E7%AB%A5%E5%B9%B4%E5%88%9B%E4%BC%A4%E7%9A%84%E5%9B%BE%E7%89%87.414992560_oscu8m.mp3'
-
-},
-
-
-
 {
   name: '4',
   singer: "4",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638916769/zz/weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8___I_lost_more_than_my_soul.._weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8___I_lost_more_than_my_soul..445156094_m5s5rk.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1639436302/zz/_-__-___-------_qckzd1.mp3'
 
 },
-
 
 
 {
   name: '5',
   singer: "5",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638916766/zz/weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8______I_don_t_like_to_make_a_fire_here.._weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8______I_don_t_like_to_make_a_fire_here..432369870_r7p1ui.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638916774/zz/%E4%BC%A4%E6%A0%B8Traumacore_%E8%AE%A9%E4%BA%BA%E5%BC%95%E8%B5%B7%E7%AB%A5%E5%B9%B4%E5%88%9B%E4%BC%A4%E7%9A%84%E5%9B%BE%E7%89%87._%E4%BC%A4%E6%A0%B8Traumacore_%E8%AE%A9%E4%BA%BA%E5%BC%95%E8%B5%B7%E7%AB%A5%E5%B9%B4%E5%88%9B%E4%BC%A4%E7%9A%84%E5%9B%BE%E7%89%87.414992560_oscu8m.mp3'
 
 },
 
@@ -67,7 +63,7 @@ const audioList1 = [
   name: '6',
   singer: "6",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915901/zz/c020109703_-_A22._%E3%81%B2%E3%81%AA%E3%81%9F%E3%81%BC%E3%81%A3%E3%81%93_o6q0tu.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638916769/zz/weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8___I_lost_more_than_my_soul.._weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8___I_lost_more_than_my_soul..445156094_m5s5rk.mp3'
 
 },
 
@@ -77,7 +73,7 @@ const audioList1 = [
   name: '7',
   singer: "7",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915899/zz/c020109703_-_A21._%E3%82%84%E3%81%95%E3%81%97%E3%81%84%E3%81%93%E3%81%A0%E3%81%A8%E3%81%8A%E3%82%82%E3%81%A3%E3%81%A6%E3%81%9F_%E3%83%9D%E3%83%8B%E5%AD%90_mhdu7i.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638916766/zz/weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8______I_don_t_like_to_make_a_fire_here.._weirdcore_dreamcore_Traumacore_%E6%80%AA%E6%A0%B8_%E6%A2%A6%E6%A0%B8_%E5%88%9B%E4%BC%A4%E6%A0%B8______I_don_t_like_to_make_a_fire_here..432369870_r7p1ui.mp3'
 
 },
 
@@ -87,7 +83,7 @@ const audioList1 = [
   name: '8',
   singer: "8",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915899/zz/c020109703_-_A02._%E3%82%86%E3%82%81%E3%81%AB%E3%81%A3%E3%81%8D.mp3_mctvmb.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915901/zz/c020109703_-_A22._%E3%81%B2%E3%81%AA%E3%81%9F%E3%81%BC%E3%81%A3%E3%81%93_o6q0tu.mp3'
 
 },
 
@@ -97,7 +93,7 @@ const audioList1 = [
   name: '9',
   singer: "9",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915898/zz/c020109703_-_A55._cuomlt.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915899/zz/c020109703_-_A21._%E3%82%84%E3%81%95%E3%81%97%E3%81%84%E3%81%93%E3%81%A0%E3%81%A8%E3%81%8A%E3%82%82%E3%81%A3%E3%81%A6%E3%81%9F_%E3%83%9D%E3%83%8B%E5%AD%90_mhdu7i.mp3'
 
 },
 
@@ -107,7 +103,7 @@ const audioList1 = [
   name: '10',
   singer: "10",
   musicSrc:
-    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915898/zz/c020109703_-_A07._%E3%81%95%E3%82%80%E3%81%8F%E3%81%A6%E3%81%82%E3%81%A3%E3%81%9F%E3%81%8B%E3%81%84_%E3%81%B5%E3%82%86_%E9%9B%AA%E3%81%AE%E4%B8%96%E7%95%8C_.mp3_d45jwk.mp3'
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915899/zz/c020109703_-_A02._%E3%82%86%E3%82%81%E3%81%AB%E3%81%A3%E3%81%8D.mp3_mctvmb.mp3'
 
 },
 
@@ -117,9 +113,59 @@ const audioList1 = [
   name: '11',
   singer: "11",
   musicSrc:
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915898/zz/c020109703_-_A55._cuomlt.mp3'
+
+},
+
+
+
+{
+  name: '12',
+  singer: "12",
+  musicSrc:
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1638915898/zz/c020109703_-_A07._%E3%81%95%E3%82%80%E3%81%8F%E3%81%A6%E3%81%82%E3%81%A3%E3%81%9F%E3%81%8B%E3%81%84_%E3%81%B5%E3%82%86_%E9%9B%AA%E3%81%AE%E4%B8%96%E7%95%8C_.mp3_d45jwk.mp3'
+
+},
+
+
+
+{
+  name: '13',
+  singer: "13",
+  musicSrc:
     'https://res.cloudinary.com/dgemakazn/video/upload/v1638915897/zz/c020109703_-_A35._%E3%81%9F%E3%81%AE%E3%81%97%E3%81%9D%E3%81%86%E3%81%AA%E3%83%94%E3%82%AF%E3%83%8B%E3%83%83%E3%82%AF_clmcvw.mp3'
 
 },
+
+
+{
+  name: '14',
+  singer: "14",
+  musicSrc:
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1639436303/zz/---_smbkep.mp3'
+
+},
+
+
+{
+  name: '15',
+  singer: "15",
+  musicSrc:
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1639436302/zz/----_----_------_----_oabd0n.mp3'
+
+},
+
+
+
+
+{
+  name: '16',
+  singer: "16",
+  musicSrc:
+    'https://res.cloudinary.com/dgemakazn/video/upload/v1639436302/zz/__-_-__lwknje.mp3'
+
+},
+
 
 
 
